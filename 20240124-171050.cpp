6c31dd7643e4e530636e24ad64d914d5d53bf147
4072-jenish <150036720+4072-jenish@users.noreply.github.com>
2024-01-29 15:43:28 +0530

    20240124-171050.cpp

--- 20240124-171050.cpp
+++ 20240124-171050.cpp
@@ -1,29 +1,10 @@
-Skip to content
 
-Code
-Issues
-Pull requests
-BreadcrumbsC-Heritage
-/3_3.cpp
-Latest commit
-kunal-gangani
-kunal-gangani
-4 hours ago
-History
-60 lines (60 loc) · 1.19 KB
-File metadata and controls
-
-Code
-
-Blame
-Older
-Newer
-
-Added
-5 hours ago
 #include<iostream>
+
 using namespace std;
+
 class Animal{
+
 	public:
 		int age;
 		char name[25];
@@ -35,7 +16,9 @@ class Animal{
 			cout<<"I am "<<name<<" & I am "<<age<<" old."<<endl;
 		}
 };
+
 class Zebra : public Animal{
+
 	public:
 		char place[25];
 		char type[25];
@@ -48,16 +31,25 @@ class Zebra : public Animal{
 			cin>>place;
 			cout<<"Enter Zebra's Food Type : ";
 			cin>>type;
+			
 		}
+
 		void ZebraOutput(){
+			
 			get_values();
 			cout<<"Zebra's Origin Place is : "<<place<<" And It is "<<type<<endl;
+			
 		}
+
 };
-class Dolphin : public Animal{
+
+class Dolphin : public Animal
+{
+
 	public:
 		char place;
 		char type;
+
 		void DolphinInput(){
 			cout<<"Enter Dolphin's Age : ";
 			cin>>age;
@@ -68,17 +60,24 @@ class Dolphin : public Animal{
 			cout<<"Enter Dolphin's Food Type : ";
 			cin>>type;
 		}
+
 		void DolphinOutput(){
+			
 			get_values();
+			
 			cout<<"Dolphin's Origin Place is : "<<place<<" And It is "<<type<<endl;
+			
 		}
+
 };
 int main(){
+	
 	Zebra z;
 	Dolphin d;
 	z.ZebraInput();
 	z.ZebraOutput();
 	d.DolphinInput();
 	d.DolphinOutput();
+	
+	return 0;
 }
-Blaming C-Heritage/3_3.cpp at master · kunal-gangani/C-Heritage
